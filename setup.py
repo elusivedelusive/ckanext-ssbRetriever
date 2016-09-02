0c563f5e74f6b5efa81b6159c2be8566daa208e9
Jonatan Hoff <jona341@hotmail.com>
2016-09-02 15:43:09 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,17 @@ setup(
     description="Grafisk brukergrensesnitt for aa kjore POST queries mot SSB utviklet for Stavanger Kommune",
     long_description='''Contributors:
     Jonatan Hoff <jonatan.hoff@gmail.com>''',
+    author = "Jonatan Hoff",
     url="https://github.com/elusivedelusive/ckanext-ssbRetriever",
     license="",
+    namespace_packages=['ckanext', 'ckanext.ssbRetriever'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[],
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords="CKAN",
     include_package_data = True,
     entry_points='''
         [ckan.plugins]
-        ssbRetriever=ckanext.ssbRetriever.plugin:ssbRetriever
+        ssbRetriever=ckanext.ckanext-ssbRetriever.plugin:ssbRetriever
     ''',
     )
