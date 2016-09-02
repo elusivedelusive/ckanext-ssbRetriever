ad1a01bb3374fba5bba011fe8eb2a392dea0a6a9
Jonatan Hoff <jona341@hotmail.com>
2016-09-02 14:49:20 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+version = "0.1"
+
+setup(
+    name="ckanext-ssbRetriever",
+    version="0.1",
+
+    description="Grafisk brukergrensesnitt for å kjøre POST queries mot SSB utviklet for Stavanger Kommune",
+    long_description='''Contributors:
+    Jonatan Hoff <jonatan.hoff@gmail.com>''',
+    url="https://github.com/elusivedelusive/ckanext-ssbRetriever",
+    license="",
+    )
