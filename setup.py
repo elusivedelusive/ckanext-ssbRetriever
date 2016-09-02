2f6a277b1dda153c5ccc38cff92741cdccb46632
Jonatan Hoff <jona341@hotmail.com>
2016-09-02 15:11:33 +0200

    removed norwegian character

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="ckanext-ssbRetriever",
     version="0.1",
 
-    description="Grafisk brukergrensesnitt for å kjøre POST queries mot SSB utviklet for Stavanger Kommune",
+    description="Grafisk brukergrensesnitt for å kjore POST queries mot SSB utviklet for Stavanger Kommune",
     long_description='''Contributors:
     Jonatan Hoff <jonatan.hoff@gmail.com>''',
     url="https://github.com/elusivedelusive/ckanext-ssbRetriever",
