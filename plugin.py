a49b094a2a9e92ee3134b5d45080424dcb0f219a
Jonatan Hoff <jona341@hotmail.com>
2016-09-05 09:27:00 +0200

    added plugins.SingletonPlugin to plugin.py

--- plugin.py
+++ plugin.py
@@ -1,7 +1,7 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-class ssbRetriever:
+class ssbRetriever(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
     def update_config(self, config_):
