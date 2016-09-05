8f1da679fdd734886378b8230b7ab5d7c8c7004c
Jonatan Hoff <jona341@hotmail.com>
2016-09-05 15:25:24 +0200

    html test

--- plugin.py
+++ plugin.py
@@ -3,7 +3,7 @@ import ckan.plugins.toolkit as toolkit
 import requests
 
 def query_form():
-	return "the end is coming"
+    return "the end is coming"
 
 class ssbRetriever(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -13,6 +13,33 @@ class ssbRetriever(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
 
     def get_helpers(self):
-	return {'ssbRetriever_query_form': query_form}
+        return {'ssbRetriever_query_form': query_form}
 
+    def package_form(self):
+        return 'package/new_package_form.html'
 
+    def new_template(self):
+        return 'package/new.html'
+
+    def comments_template(self):
+        return 'package/comments.html'
+
+    def search_template(self):
+        return 'package/search.html'
+
+    def read_template(self):
+        return 'package/read.html'
+
+    def history_template(self):
+        return 'package/history.html'
+
+    def package_types(self):
+        return ['dataset']
+
+    def is_fallback(self):
+        return True
+
+    def setup_template_variables(self, context, data_dict=None, package_type=None):
+        from ckan.lib.base import c
+        from ckan import model
+        c.licences = model.Package.get_license_options()
