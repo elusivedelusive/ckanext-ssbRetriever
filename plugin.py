416aebd1f404c2d619e723b127c1b8c93d690fe8
Jonatan Hoff <jona341@hotmail.com>
2016-09-09 09:20:09 +0200

    fixed formatting

--- plugin.py
+++ plugin.py
@@ -1,25 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 from ckan.controllers.package import PackageController
 
 
-class NewSSBResource(PackageController):
-	resource_form = 'custom_resource_form.html'
-
 class ssbRetriever(plugins.SingletonPlugin):
+
     plugins.implements(plugins.IConfigurer)
     plugins.implements(IRoutes, inherit=True)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
-		implements(IRoutes, inherit=True)
 
- 	def before_map(self, map):
-		map.connect('/SSB/new',
-        controller='ckanext.ckanext-ssbRetriever.controllers.controller:SSBController',
-		action='new_resource')
-		return map
+    def before_map(self, map):
+        map.connect('/SSB/new',
+                    controller='ckanext.ckanext-ssbRetriever.controllers.controller:SSBController'
+                    , action='new_resource')
+        return map
 
-	def after_map(self, map):
-		return map
+    def after_map(self, map):
+        return map
