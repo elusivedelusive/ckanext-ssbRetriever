f1b45f72bfa0de719e7f1e8f9248b016ee9e788d
Jonatan Hoff <jonatan.hoff@gmail.com>
2016-09-09 20:43:28 +0200

    working full stack copy

--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
 import requests
-import json
 
-def execute_query(query, url):
-	r = requests.get(url)
-	return r
+#performs a simple post request using the python requests library
+#url = url to an endpoint that accepts post requests
+#data = request body. In the case of querying SSB it contains a json query
+def execute_simple_post_query(url, query):
+	return requests.post(url, data = query)
+
+#performs a multipart/form request using the python requests library
+#url = url to an endpoint that accepts post requests
+#files = one or several files to be uploaded
+#headers = requests headers
+#data = various parameters that the endpoint may require
+def multipart_post(url, files, headers, params):
+	return requests.post(url, files = files, headers = headers, data = params)
